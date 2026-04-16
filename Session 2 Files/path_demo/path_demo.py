34e77acff7dab789ee9adca71d36264fc020a8f4
IridescentBlaze <iridescentblazer@gmail.com>
2026-04-16 15:40:27 -0400

    Fixed path demo error

--- Session 2 Files/path_demo/path_demo.py
+++ Session 2 Files/path_demo/path_demo.py
@@ -1,9 +1,11 @@
 import json
 from pathlib import Path # To use the class Path, we need to import it
 
-# You create a Path object by just inserting a path of either OS
+# For Windows, you'd write the path like this
 user_data_file = 'data\\users.json'
-path_user_data_file = Path(user_data_file)
+
+# You instead create a Path object by building the path manually using /
+path_user_data_file = Path('data') / 'users.json'
 
 def read_file(file_path):
     # The same function is used as before
@@ -17,6 +19,10 @@ def read_file(file_path):
 # Both will execute fine on Windows. How about Linux?
 try:
     read_file(user_data_file)
+except Exception as e:
+    print(f'An exception occurred: {e}')
+
+try:
     read_file(path_user_data_file)
 except Exception as e:
     print(f'An exception occurred: {e}')
